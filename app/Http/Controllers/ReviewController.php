b9ce16f21414daf530eb7d418d0229cd71f378cc
Piotr Proc <piotr.proc@gmail.com>
2016-03-17 18:18:38 +0100

    Adding links to Badge classes

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,131 +2,142 @@
 
 use App\Http\Requests;
 use App\Project;
+use App\Services\Analyzer\Gerrit\Badges\AbstractBadge;
 use App\Services\AnalyzerInterface;
 
 class ReviewController extends Controller
 {
-	private $analyzerService;
+    private $analyzerService;
 
-	function __construct(AnalyzerInterface $analyzerService)
-	{
-		$this->analyzerService = $analyzerService;
-		//$this->middleware('auth');
-	}
+    function __construct(AnalyzerInterface $analyzerService)
+    {
+        $this->analyzerService = $analyzerService;
+        //$this->middleware('auth');
+    }
 
-	public function index()
-	{
-		$projects = array_map(function($item){
-			return $item->label;
-		}, Project::all()->getDictionary());
+    public function index()
+    {
+        $projects = array_map(function ($item) {
+            return $item->label;
+        }, Project::all()->getDictionary());
 
-		return view('review.index', [
-			'projects' => $projects,
-			'from' => date('d-m-Y'),
-			'to' => date('d-m-Y'),
-		]);
-	}
+        return view('review.index', [
+            'projects' => $projects,
+            'from' => date('d-m-Y'),
+            'to' => date('d-m-Y'),
+        ]);
+    }
 
-	public function generate(Requests\AnalyzeRequest $request, $id = null)
-	{
-		$id = $id ?: $request->get('project');
-		/** @var Project $project */
-		$project = Project::findOrFail($id);
+    public function generate(Requests\AnalyzeRequest $request, $id = null)
+    {
+        $id = $id ?: $request->get('project');
+        /** @var Project $project */
+        $project = Project::findOrFail($id);
 
-		$from = date('Y-m-d', strtotime($request->get('from')));
-		$to = date('Y-m-d', strtotime($request->get('to')));
+        $from = date('Y-m-d', strtotime($request->get('from')));
+        $to = date('Y-m-d', strtotime($request->get('to')));
 
-		$results = $this->analyzerService->analyze($project, $from, $to);
+        $results = $this->analyzerService->analyze($project, $from, $to);
 
-		\Session::set('results.'.$project->getAttribute('id'), [
-			'results' => $results,
-			'from' => $request->get('from'),
-			'to' => $request->get('to'),
-		]);
-		flash()->success('Dane projektu zostały poprawnie przetworzone.');
+        \Session::set('results.' . $project->getAttribute('id'), [
+            'results' => $results,
+            'from' => $request->get('from'),
+            'to' => $request->get('to'),
+        ]);
+        flash()->success('Dane projektu zostały poprawnie przetworzone.');
 
-		return redirect()->route('review.results', $project->getAttribute('id'));
-	}
-        
-        public function generateApi($name, $from, $to)
-	{
-            //proper format 2015-01-16
-            //echo str_replace('&2F;', '/', $name);exit;
-            $project = Project::where('name', str_replace('&2F;', '/', $name))->firstOrFail();
+        return redirect()->route('review.results', $project->getAttribute('id'));
+    }
 
-            $this->analyzerService->reBuildAnalyzerForApi();
-            $results = $this->analyzerService->analyze($project, $from, $to);
-            
-            return  $results;
-        }
-        
-        public function getBadges($projectName, $userEmail)
-	{
-            $from = date('Y-m-d', strtotime("-1 week"));;
-            $to = date("Y-m-d", time() + 86400);
-            $dataFromLastWeek = $this->generateApi($projectName, $from, $to);
+    public function generateApi($name, $from, $to)
+    {
+        //proper format 2015-01-16
+        //echo str_replace('&2F;', '/', $name);exit;
+        $project = Project::where('name', str_replace('&2F;', '/', $name))->firstOrFail();
 
-            $results_classess = [
-                'first_position_in_rank' => new \App\Services\Analyzer\Gerrit\Badges\BiggestProgessInRankingBadge(),
-                'inna_odznaka' => new \App\Services\Analyzer\Gerrit\Badges\BiggestProgessInRankingBadge()
-            ];
-            
-            $results = [];
-            
-            foreach ($results_classess as $type => $badge) {
-                $results[$type] = $badge->getBadge($dataFromLastWeek, $userEmail);	
+        $this->analyzerService->reBuildAnalyzerForApi();
+        $results = $this->analyzerService->analyze($project, $from, $to);
+
+        return $results;
+    }
+
+    public function getBadges($projectName, $userEmail)
+    {
+        $from = date('Y-m-d', strtotime("-1 week"));;
+        $to = date("Y-m-d", time() + 86400);
+        $dataFromLastWeek = $this->generateApi($projectName, $from, $to);
+
+        $badges = array(
+            new \App\Services\Analyzer\Gerrit\Badges\BiggestProgessInRankingBadge(),
+            new \App\Services\Analyzer\Gerrit\Badges\HardWorkingBadge(),
+            new \App\Services\Analyzer\Gerrit\Badges\ManyChangesInOneDayBadge(),
+            new \App\Services\Analyzer\Gerrit\Badges\ManyReviewsInOneDayBadge(),
+            new \App\Services\Analyzer\Gerrit\Badges\MostChangesBadge(),
+            new \App\Services\Analyzer\Gerrit\Badges\MostCommentsPerChangeBadge(),
+            new \App\Services\Analyzer\Gerrit\Badges\MostReviewsBadge(),
+            new \App\Services\Analyzer\Gerrit\Badges\PerfectQualityChangeBadge(),
+            new \App\Services\Analyzer\Gerrit\Badges\PourQualityChangeBadge()
+        );
+
+        $rewardedBadges = [];
+
+        foreach ($badges as $index => $badge) {
+            /** @var AbstractBadge $badge */
+            if ($badge->getBadge($dataFromLastWeek, $userEmail)) {
+                $rewardedBadges[$index] = $badge;
             }
-            
-            return $results;
         }
 
-	public function analyze($id)
-	{
-		/** @var Project $project */
-		$project = Project::findOrFail($id);
-		$analyzers = $this->analyzerService->getList()[$project->getType()];
+        return $rewardedBadges;
+    }
 
-		return view('review.analyze', [
-			'project' => $project,
-			'analyzers' => $analyzers,
-			'from' => date('d-m-Y'),
-			'to' => date('d-m-Y'),
-		]);
-	}
+    public function analyze($id)
+    {
+        /** @var Project $project */
+        $project = Project::findOrFail($id);
+        $analyzers = $this->analyzerService->getList()[$project->getType()];
 
-	public function results($id)
-	{
-		/** @var Project $project */
-		$project = Project::findOrFail($id);
-		$analyzers = $this->analyzerService->getList()[$project->getType()];
-		$rankers = $this->analyzerService->getRankers()[$project->getType()];
-		$results = \Session::get('results.'.$project->getAttribute('id'), [
-			'from' => date('d-m-Y'),
-			'to' => date('d-m-Y'),
-			'results' => [],
-		]);
-		$tabs = [
-			'changes' => 'Zmiany',
-			'comments' => 'Komentarze',
-			'statistics' => 'Statystyki',
-			'topics' => 'Tematy',
-			'pairs' => 'Pary',
-		];
+        return view('review.analyze', [
+            'project' => $project,
+            'analyzers' => $analyzers,
+            'from' => date('d-m-Y'),
+            'to' => date('d-m-Y'),
+        ]);
+    }
 
-		if (empty($results['results'])) {
-			flash()->error('Brak wyników. Spróbuj ponownie.');
+    public function results($id)
+    {
+        /** @var Project $project */
+        $project = Project::findOrFail($id);
+        $analyzers = $this->analyzerService->getList()[$project->getType()];
+        $rankers = $this->analyzerService->getRankers()[$project->getType()];
+        $results = \Session::get('results.' . $project->getAttribute('id'), [
+            'from' => date('d-m-Y'),
+            'to' => date('d-m-Y'),
+            'results' => [],
+        ]);
+        $tabs = [
+            'changes' => 'Zmiany',
+            'comments' => 'Komentarze',
+            'statistics' => 'Statystyki',
+            'topics' => 'Tematy',
+            'pairs' => 'Pary',
+        ];
 
-			return redirect()->route('review.analyze', $project->getAttribute('id'));
-		}
+        if (empty($results['results'])) {
+            flash()->error('Brak wyników. Spróbuj ponownie.');
 
-		return view('review.results', [
-			'project' => $project,
-			'tabs' => $tabs,
-			'analyzers' => $analyzers,
-			'rankers' => $rankers,
-			'from' => $results['from'],
-			'to' => $results['to'],
-			'results' => $results['results'],
-		]);
-	}
+            return redirect()->route('review.analyze', $project->getAttribute('id'));
+        }
+
+        return view('review.results', [
+            'project' => $project,
+            'tabs' => $tabs,
+            'analyzers' => $analyzers,
+            'rankers' => $rankers,
+            'from' => $results['from'],
+            'to' => $results['to'],
+            'results' => $results['results'],
+        ]);
+    }
 }
--- app/Services/Analyzer/Gerrit/Badges/AbstractBadge.php
+++ app/Services/Analyzer/Gerrit/Badges/AbstractBadge.php
@@ -4,14 +4,15 @@ namespace App\Services\Analyzer\Gerrit\Badges;
 
 abstract class AbstractBadge
 {
-	protected $writtenIcon;
-	protected $description;
+	public $icon;
+	public $description;
 
-	public function __construct($writtenIcon, $description)
+	public function __construct($icon, $description)
 	{
-		$this->writtenIcon = $writtenIcon;
+		$this->icon = $icon;
 		$this->description = $description;
 	}
 
 	public abstract function getBadge($results, $email);
+
 }
