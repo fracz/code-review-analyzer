80816e08c51aad0fc162c2762db67efd94553c30
Piotr Proc <piotr.proc@gmail.com>
2016-03-17 15:34:40 +0100

    Empty commmit

--- app/Services/Analyzer/Gerrit/Badges/FirstPositionInRank.php
+++ app/Services/Analyzer/Gerrit/Badges/FirstPositionInRank.php
@@ -5,8 +5,10 @@ namespace App\Services\Analyzer\Gerrit\Badges;
 class FirstPositionInRank extends AbstractBadge
 {
     public function getBadge($results, $email) {
+        print_r($results);
+
         // w $results masz analize danego projektu z ostatniego tygodnia
-        return "AA";
+        return "AAA";
     }
 }
 
