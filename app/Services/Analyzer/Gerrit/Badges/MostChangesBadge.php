8096f0b7dba675ee71d3bd5c0a02d3a0fa6e4bbd
Piotr Proc <piotr.proc@gmail.com>
2016-03-30 09:06:37 +0200

    Colorfull awesome fonts

--- app/Services/Analyzer/Gerrit/Badges/MostChangesBadge.php
+++ app/Services/Analyzer/Gerrit/Badges/MostChangesBadge.php
@@ -14,7 +14,7 @@ class MostChangesBadge extends AbstractOnePropertyBadge
 
     public function __construct()
     {
-        parent::__construct("☆", "<i class=\"fa fa-random\" style=\"color:coral\"></i>", "You've made most of the commits in project this week",
+        parent::__construct("☆", "<i class=\"fa fa-random\" style=\"color:teal\"></i>", "You've made most of the commits in project this week",
             "commits_per_user", "count");
     }
 
