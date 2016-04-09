88ef98b027511c4f6c25f505e0bb147718bb393d
Piotr Proc <piotr.proc@gmail.com>
2016-04-09 13:13:26 +0200

    reback badge cache

--- app/Http/Controllers/BadgeController.php
+++ app/Http/Controllers/BadgeController.php
@@ -51,11 +51,11 @@ class BadgeController extends Controller
     {
 		session_write_close();
 		
-        //if (Cache::has('cachedBadges-'.$projectName.'-'.$userEmail.'-'.$from.'-'.$to)) {
+        if (Cache::has('cachedBadges-'.$projectName.'-'.$userEmail.'-'.$from.'-'.$to)) {
 			
-            //return Cache::get('cachedBadges-'.$projectName.'-'.$userEmail.'-'.$from.'-'.$to);
+            return Cache::get('cachedBadges-'.$projectName.'-'.$userEmail.'-'.$from.'-'.$to);
 
-        //} else {
+        } else {
             $dataFromLastWeek = $this->generateApi($projectName, $from, $to);
 
 			if($dataFromLastWeek == null){
@@ -89,7 +89,7 @@ class BadgeController extends Controller
             Cache::put('cachedBadges-'.$projectName.'-'.$userEmail.'-'.$from.'-'.$to, $api, 10);
 
             return $api;
-        //}
+        }
     }
 
     public function compareBadges($badgeA, $badgeB)
