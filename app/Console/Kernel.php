15ee7d06aa92989e203d0ed2e8abd30c64ec8ebe
Wojciech Frącz <fraczwojciech@gmail.com>
2017-03-17 13:37:52 +0100

    Update url in schedule run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,13 +1,14 @@
 <?php namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Project;
 use App\Person;
+use App\Project;
 use App\Services\DataFetching\GerritDataFetchingTrait;
 use Cache;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel {
+class Kernel extends ConsoleKernel
+{
 
     use GerritDataFetchingTrait;
 
@@ -17,93 +18,94 @@ class Kernel extends ConsoleKernel {
      * @var array
      */
     protected $commands = [
-            'App\Console\Commands\Inspire',
+        'App\Console\Commands\Inspire',
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-            $schedule->command('inspire')
-                             ->hourly();
+        $schedule->command('inspire')
+            ->hourly();
 
-            $schedule->call(function () {
-				Cache::put('emails-to-update', [], 120);
-				Cache::put('emails-to-update-from-badges', [], 120);
-				
-				
+        $schedule->call(function () {
+            Cache::put('emails-to-update', [], 120);
+            Cache::put('emails-to-update-from-badges', [], 120);
+
+
+            $projects = Project::all();
+
+            foreach ($projects as $project) {
+                $this->collectDataForReview($project, null, date('Y-m-d', strtotime(date("Y-m-d") . "+1 days")));
+            }
+
+            print_r("data stored...");
+            echo "<br/><br/>Emails to update after collecting data: ";
+            print_r(Cache::get('emails-to-update'));
+
+            $emails = Cache::get('emails-to-update');
+            foreach ($emails as $email) {
+                echo "<br/><br/> getting data for " . $email . "<br/>";
+
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, "https://review-analyzer.fslab.agh.edu.pl/review/api/badges/user/nocache/" . $email);
+
+                curl_exec($ch);
+                curl_close($ch);
+            }
+
+
+            echo "<br/><br/>Emails to update from badge changes: ";
+            print_r(Cache::get('emails-to-update-from-badges'));
+
+            $emails = Cache::get('emails-to-update-from-badges');
+            foreach ($emails as $email) {
+                echo "<br/><br/> getting data for " . $email . "<br/>";
+
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, "https://review-analyzer.fslab.agh.edu.pl/review/api/badges/user/nocache/" . $email);
+
+                curl_exec($ch);
+                curl_close($ch);
+            }
+
+        })->everyFiveMinutes();
+
+        $schedule->call(function () {
+
+            $today = date("H:i:s");
+            $today_exploded = explode(":", $today);
+            echo "No need to refresh all cache, hour = " . $today_exploded[0];
+
+            if ($today_exploded[0] == "03") {
+                echo "EXECUTING REFFRESH ALL CACHE";
+
                 $projects = Project::all();
 
-                foreach ($projects as $project){
-                    $this->collectDataForReview($project, null, date('Y-m-d',strtotime(date("Y-m-d") . "+1 days")));
+                foreach ($projects as $project) {
+                    $this->collectDataForReview($project, null, date('Y-m-d', strtotime(date("Y-m-d") . "+1 days")));
                 }
-				
-				print_r("data stored...");
-				echo "<br/><br/>Emails to update after collecting data: ";
-				print_r(Cache::get('emails-to-update'));
-				
-				$emails = Cache::get('emails-to-update');
-				foreach($emails as $email){
-					echo "<br/><br/> getting data for " . $email . "<br/>";
-					
-					$ch = curl_init(); 
-					curl_setopt($ch, CURLOPT_URL, "http://apps.iisg.agh.edu.pl:10005/review/api/badges/user/nocache/".$email);
-					
-					curl_exec($ch);
-					curl_close($ch);  
-				}
-				
-				
-				echo "<br/><br/>Emails to update from badge changes: ";
-				print_r(Cache::get('emails-to-update-from-badges'));
-				
-				$emails = Cache::get('emails-to-update-from-badges');
-				foreach($emails as $email){
-					echo "<br/><br/> getting data for " . $email . "<br/>";
-					
-					$ch = curl_init(); 
-					curl_setopt($ch, CURLOPT_URL, "http://apps.iisg.agh.edu.pl:10005/review/api/badges/user/nocache/".$email);
-					
-					curl_exec($ch);
-					curl_close($ch);  
-				}
 
-            })->everyFiveMinutes();
-			
-			$schedule->call(function () {
-				
-				$today = date("H:i:s");
-				$today_exploded = explode(":", $today);
-				echo "No need to refresh all cache, hour = ".$today_exploded[0];
-				
-				if($today_exploded[0] == "03"){
-					echo "EXECUTING REFFRESH ALL CACHE";
-				
-					$projects = Project::all();
+                $persons = Person::all();
+                foreach ($persons as $person) {
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, "https://review-analyzer.fslab.agh.edu.pl/review/api/badges/user/nocache/" . $person->email);
 
-					foreach ($projects as $project){
-						$this->collectDataForReview($project, null, date('Y-m-d',strtotime(date("Y-m-d") . "+1 days")));
-					}
+                    curl_exec($ch);
+                    curl_close($ch);
+                }
 
-					$persons = Person::all();
-					foreach($persons as $person){
-						$ch = curl_init(); 
-						curl_setopt($ch, CURLOPT_URL, "http://apps.iisg.agh.edu.pl:10005/review/api/badges/user/nocache/".$person->email);
-						
-						curl_exec($ch);
-						curl_close($ch);  
-					}
+            }
 
-				}
-								
-            })->hourly();
+        })->hourly();
     }
 
-    protected function decode($result) {
+    protected function decode($result)
+    {
         return json_decode(substr($result, 4));
     }
 
