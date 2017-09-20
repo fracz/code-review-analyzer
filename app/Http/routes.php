db16dd25f2e836112f85dc829516b0d0b6239b5d
Wojciech Frącz <fraczwojciech@gmail.com>
2017-09-20 09:50:36 +0200

    Disable admin-problematic endpoints

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,11 @@ Route::get('/review', ['as' => 'review.index', 'uses' => 'ReviewController@index
 Route::get('/review/{id}/results', ['as' => 'review.results', 'uses' => 'ReviewController@results'])->where('id', '[0-9]+');
 Route::post('/review/{id?}', ['as' => 'review.generate', 'uses' => 'ReviewController@generate'])->where('id', '[0-9]+');
 
-Route::get('/review/api/cron/', ['middleware' => 'cors', 'as' => 'review.cron', 'uses' => 'ReviewController@runCron']);
+//Route::get('/review/api/cron/', ['middleware' => 'cors', 'as' => 'review.cron', 'uses' => 'ReviewController@runCron']);
 
-Route::get('/review/api/allprojects/', ['middleware' => 'cors', 'as' => 'review.allprojects', 'uses' => 'ReviewController@getAllProjects']);
+//Route::get('/review/api/allprojects/', ['middleware' => 'cors', 'as' => 'review.allprojects', 'uses' => 'ReviewController@getAllProjects']);
 Route::get('/review/api/badges/days/{projectName}', ['middleware' => 'cors', 'as' => 'review.userbadgesdays', 'uses' => 'BadgeController@getDaysForProject']);
-Route::get('/review/api/badges/user', ['middleware' => 'cors', 'as' => 'review.alluserbadges', 'uses' => 'BadgeController@getAllUserBadges']);
+//Route::get('/review/api/badges/user', ['middleware' => 'cors', 'as' => 'review.alluserbadges', 'uses' => 'BadgeController@getAllUserBadges']);
 Route::get('/review/api/badges/user/{userEmail}', ['middleware' => 'cors', 'as' => 'review.userbadges', 'uses' => 'BadgeController@getUserBadges']);
 Route::get('/review/api/badges/user/nocache/{userEmail}', ['middleware' => 'cors', 'as' => 'review.userbadgesnocache', 'uses' => 'BadgeController@getUserBadgesWithoutCache']);
 Route::get('/review/api/badges/{projectName}/{userEmail}', ['middleware' => 'cors', 'as' => 'review.badges', 'uses' => 'BadgeController@getBadges']);
